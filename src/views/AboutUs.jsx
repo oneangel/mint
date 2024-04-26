a7222254c7fd340b688ad728942726ab1c29a282
Angel Fuentes <angel09reyes29@gmail.com>
2024-04-25 23:10:26 -0600

    archivo about us

--- src/views/AboutUs.jsx
+++ src/views/AboutUs.jsx
@@ -6,7 +6,8 @@ export const AboutUs = () => {
     <div>
       <Header />
       
-      <h1>La mejor manera de controlar tus ganancias</h1>
+      <h1 className="font-semibold text-center text-7xl">La mejor manera de controlar tus ganancias</h1>
+      <p className="text-xl text-center">Mint te ayuda a controlar tus finanzas de una manera agil y sencilla para mantener una salud financiera.</p>
     </div>
   );
 };
