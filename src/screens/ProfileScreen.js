8a0a0d5bd1e3361578ed67205a918ddf1d403cf5
Angel Fuentes <angel09reyes29@gmail.com>
2024-04-27 15:20:08 -0600

    edit profile inputs

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -6,22 +6,25 @@ import {
   TouchableOpacity,
   StyleSheet,
   ScrollView,
+  Pressable
 } from "react-native";
 import { Image } from "expo-image";
+import { Input, Icon } from "native-base";
+import {MaterialIcons} from "@expo/vector-icons";
 
 const ProfileScreen = () => {
+  const [show, setShow] = useState(false);
   const [editing, setEditing] = useState(false);
   const [name, setName] = useState("Eduardo");
   const [email, setEmail] = useState("lalo@gmail.com");
   const [phone, setPhone] = useState("123-456-7890");
   const [birthdate, setBirthdate] = useState("1990-01-01"); // Estado para la fecha de nacimiento
-  const [password, setPassword] = useState("********"); // Estado para la contraseña
+  const [password, setPassword] = useState("lalo1234"); // Estado para la contraseña
   const [profileImage, setProfileImage] = useState(
     "https://s.france24.com/media/display/451ed2b8-eed6-11ea-afdd-005056bf87d6/w:980/p:16x9/messi-1805.jpg"
   );
 
   const handleSave = () => {
-    // Aquí puedes agregar la lógica para guardar los cambios
     setEditing(false);
   };
 
@@ -38,66 +41,110 @@ const ProfileScreen = () => {
       <View style={styles.container}>
         <View style={styles.profileHeader}>
           <TouchableOpacity onPress={handleEditImage}>
-            <Image source={profileImage} style={styles.profileImage} transition={1000}/>
+            <Image
+              source={profileImage}
+              style={styles.profileImage}
+              transition={1000}
+            />
           </TouchableOpacity>
           <Text style={styles.name}>{name}</Text>
         </View>
         <View style={styles.profileDetails}>
           <Text style={styles.label}>Nombre:</Text>
           {editing ? (
-            <TextInput
+            <Input
+              variant="rounded"
               style={styles.input}
               value={name}
               onChangeText={setName}
               placeholder="Nombre"
             />
           ) : (
-            <Text style={styles.text}>{name}</Text>
+            <Input
+              variant="rounded"
+              value={name}
+              isDisabled={true}
+            />
           )}
           <Text style={styles.label}>Correo Electrónico:</Text>
           {editing ? (
-            <TextInput
+            <Input
+              variant="rounded"
               style={styles.input}
               value={email}
               onChangeText={setEmail}
               placeholder="Correo Electrónico"
             />
           ) : (
-            <Text style={styles.text}>{email}</Text>
+            <Input
+              variant="rounded"
+              value={email}
+              isDisabled={true}
+            />
           )}
           <Text style={styles.label}>Teléfono:</Text>
           {editing ? (
-            <TextInput
+            <Input
+              variant="rounded"
               style={styles.input}
               value={phone}
               onChangeText={setPhone}
               placeholder="Teléfono"
             />
           ) : (
-            <Text style={styles.text}>{phone}</Text>
+            <Input
+              variant="rounded"
+              value={phone}
+              isDisabled={true}
+            />
           )}
           <Text style={styles.label}>Fecha de Nacimiento:</Text>
           {editing ? (
-            <TextInput
+            <Input
+              variant="rounded"
               style={styles.input}
               value={birthdate}
               onChangeText={setBirthdate}
               placeholder="Fecha de Nacimiento"
             />
           ) : (
-            <Text style={styles.text}>{birthdate}</Text>
+            <Input
+              variant="rounded"
+              value={birthdate}
+              isDisabled={true}
+            />
           )}
           <Text style={styles.label}>Contraseña:</Text>
           {editing ? (
-            <TextInput
+            <Input
+              variant="rounded"
               style={styles.input}
               value={password}
               onChangeText={setPassword}
               placeholder="Contraseña"
-              secureTextEntry={true}
+              type={show ? "text" : "password"}
+              InputRightElement={
+                <Pressable onPress={() => setShow(!show)}>
+                  <Icon
+                    as={
+                      <MaterialIcons
+                        name={show ? "visibility" : "visibility-off"}
+                      />
+                    }
+                    size={5}
+                    mr="2"
+                    color="muted.400"
+                  />
+                </Pressable>
+              }
             />
           ) : (
-            <Text style={styles.text}>********</Text>
+            <Input
+              variant="rounded"
+              value={password}
+              isDisabled={true}
+              type="password"
+            />
           )}
         </View>
         {editing && (
@@ -116,8 +163,8 @@ const ProfileScreen = () => {
           </TouchableOpacity>
         )}
         <TouchableOpacity style={styles.logOutButton}>
-            <Text style={styles.buttonText}>Cerrar Sesión</Text>
-          </TouchableOpacity>
+          <Text style={styles.buttonText}>Cerrar Sesión</Text>
+        </TouchableOpacity>
       </View>
     </ScrollView>
   );
@@ -155,14 +202,7 @@ const styles = StyleSheet.create({
     fontSize: 16,
     marginBottom: 15,
   },
-  input: {
-    fontSize: 16,
-    borderWidth: 1,
-    borderColor: "#ccc",
-    borderRadius: 20,
-    padding: 10,
-    marginBottom: 15,
-  },
+  input: {},
   buttonsContainer: {
     flexDirection: "row",
     justifyContent: "space-between",
@@ -192,7 +232,7 @@ const styles = StyleSheet.create({
     paddingVertical: 10,
     paddingHorizontal: 20,
     alignItems: "center",
-    marginTop: 10
+    marginTop: 10,
   },
   buttonText: {
     color: "#fff",
